13463c34f00722350069ad1999c8413416f806f7
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2019-05-15 03:14:14 +0000

    Space placeholder string in CSVRecord

--- src/CSVRecord.js
+++ src/CSVRecord.js
@@ -8,13 +8,13 @@ const styles = {
 };
 
 const placeholder =
-  "0,tcp,http,SF,184,124,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,1,0.00,0.00,0.00,0.00,1.00,0.00,0.00,10,10,1.00,0.00,0.10,0.00,0.00,0.00,0.00,0.00";
+  "0, tcp, http, SF, 184, 124, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0.00, 0.00, 0.00, 0.00, 1.00, 0.00, 0.00, 10, 10, 1.00, 0.00, 0.10, 0.00, 0.00, 0.00, 0.00, 0.00";
 
 function CSVRecord(props) {
   const { classes } = props;
   return (
     <Grid container justify="center">
-      <Grid item sm={10}>
+      <Grid item sm={11}>
         <TextField
           id="outlined-with-placeholder"
           label="CSV Record"
