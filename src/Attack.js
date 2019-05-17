e85a0424f2dc67b6ac903aa82d102a4983a9e3ee
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2019-05-17 00:12:07 +0000

    Send features to Attack view

--- src/Attack.js
+++ src/Attack.js
@@ -3,10 +3,13 @@ import React from "react";
 import { Typography } from "@material-ui/core";
 
 function Attack(props) {
-  const { value } = props;
+  const { features } = props;
+  var prediction = 1;
   return (
     <div>
-      <Typography variant="h3">{value ? "Malicious" : "Normal"}</Typography>
+      <Typography variant="h3">
+        {prediction ? "Malicious" : "Normal"}
+      </Typography>
     </div>
   );
 }
