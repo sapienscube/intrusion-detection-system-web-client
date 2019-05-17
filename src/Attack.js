bbf46322c40d6f37e1ce56031823ee5b5eb9a4a3
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2019-05-17 18:03:56 +0000

    Ready to implement model.js: must return 1 or 0 for malicious vs normal

--- src/Attack.js
+++ src/Attack.js
@@ -4,20 +4,25 @@ import { Typography } from "@material-ui/core";
 
 import predict from "./model";
 
-function Attack(props) {
+function getArray(features) {
   let nums = [];
   let cats = [];
-  props.features.numerical.titles.map(
+  features.numerical.titles.map(
     title => (nums = nums.concat(title.placeholder))
   );
-  props.features.categorical.map(cat => (cats = cats.concat(cat.placeholder)));
-  const features = [nums[0]].concat(cats).concat(nums.slice(1));
-  console.log(features);
+  features.categorical.map(cat => (cats = cats.concat(cat.placeholder)));
+  return [nums[0]].concat(cats).concat(nums.slice(1));
+}
 
-  predict(features).then(prediction => console.log("prediction: ", prediction));
+function Attack(props) {
+  const features = getArray(props.features);
+  console.log(features);
+  const prediction = predict(features);
   return (
     <div>
-      <Typography variant="h3">{1 ? "Malicious" : "Normal"}</Typography>
+      <Typography variant="h3">
+        {prediction == 1 ? "Malicious" : "Normal"}
+      </Typography>
     </div>
   );
 }
--- src/model.js
+++ src/model.js
@@ -1,6 +1,6 @@
 import * as tf from "@tensorflow/tfjs";
 
-async function predict(features) {
+function predict(features) {
   // JavaScript
   const model_gh_link =
     "https://crossorigin.me/https://github.com/sapienscube/kddcup-intrusion-detection-system/blob/master/src/model.json";
@@ -8,9 +8,9 @@ async function predict(features) {
   const model_gdrive_link =
     "https://crossorigin.me/https://drive.google.com/open?id=1-YAH1oKF141fSnmIfIF7N8YnUXviTlD-";
 
-  const model = await tf.loadLayersModel(model_gh_link);
-  console.log(model);
-  return 1;
+  // const model = await tf.loadLayersModel(model_gh_link);
+  // console.log(model);
+  return features[0];
 }
 
 export default predict;
