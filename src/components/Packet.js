ef0d566378b251c4caedd69834fb959740e8d0da
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2019-05-21 22:28:28 +0000

    Implemented two-way binding: Packet <=> Features

--- src/components/Packet.js
+++ src/components/Packet.js
@@ -7,8 +7,8 @@ const styles = {
   }
 };
 
-const placeholder =
-  "0,tcp,http,SF,184,124,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,1,0.00,0.00,0.00,0.00,1.00,0.00,0.00,10,10,1.00,0.00,0.10,0.00,0.00,0.00,0.00,0.00";
+// const placeholder =
+//   "0,tcp,http,SF,184,124,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,1,0.00,0.00,0.00,0.00,1.00,0.00,0.00,10,10,1.00,0.00,0.10,0.00,0.00,0.00,0.00,0.00";
 
 class Packet extends React.Component {
   state = {};
@@ -18,16 +18,29 @@ class Packet extends React.Component {
   };
   render() {
     const classes = this.props.classes;
+    const cats = [];
+    const nums = [];
+    this.props.features.categorical.map(cat => cats.push(cat.placeholder));
+
+    this.props.features.numerical.titles.map(title =>
+      nums.push(title.placeholder)
+    );
+
+    const featuresArray = nums
+      .slice(0, 1)
+      .concat(cats)
+      .concat(nums.slice(1));
+
     return (
       <Grid container justify="center">
         <Grid item sm={11}>
           <TextField
             id="outlined-with-placeholder"
             label="Network Trafic Packet"
-            defaultValue={placeholder}
             className={classes.textField}
             margin="normal"
             variant="outlined"
+            value={featuresArray}
             onChange={this.handleChange}
           />
         </Grid>
