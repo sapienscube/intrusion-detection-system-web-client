2340f46d9d6599ec06836b2404c7ae1b98a24fd2
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2019-05-22 14:48:23 +0000

    Successful query with mock features in Detector.js

--- src/components/Detector.js
+++ src/components/Detector.js
@@ -1,13 +1,76 @@
 import React from "react";
 
 import { gql } from "apollo-boost";
-import graphql from "react-apollo";
+import { Query } from "react-apollo";
 
 import { getFeaturesArray } from "./Utils";
 
 function Detector(props) {
   const features = getFeaturesArray(props.features);
-  return <div>{features[1]}</div>;
+  return (
+    <div>
+      <Query
+        query={gql`
+          {
+            predict(
+              packets: [
+                [
+                  "0"
+                  "tcp"
+                  "http"
+                  "SF"
+                  "184"
+                  "124"
+                  "0"
+                  "0"
+                  "0"
+                  "0"
+                  "0"
+                  "1"
+                  "0"
+                  "0"
+                  "0"
+                  "0"
+                  "0"
+                  "0"
+                  "0"
+                  "0"
+                  "0"
+                  "0"
+                  "1"
+                  "1"
+                  "0.00"
+                  "0.00"
+                  "0.00"
+                  "0.00"
+                  "1.00"
+                  "0.00"
+                  "0.00"
+                  "10"
+                  "10"
+                  "1.00"
+                  "0.00"
+                  "0.10"
+                  "0.00"
+                  "0.00"
+                  "0.00"
+                  "0.00"
+                  "0.00"
+                ]
+              ]
+            )
+          }
+        `}
+      >
+        {({ loading, error, data }) => {
+          if (loading) return <div>loading..</div>;
+          if (error) return <div>Error:</div>;
+          console.log(data);
+          return <div>{data.predict}</div>;
+        }}
+      </Query>
+    </div>
+  );
 }
 
 export default Detector;
