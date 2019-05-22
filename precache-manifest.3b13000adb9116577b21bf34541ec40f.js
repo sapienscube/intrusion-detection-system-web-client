91bc3dd465fda3c06239c3ede80845dcf07ff507
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2019-05-22 01:47:29 +0000

    Updates

---
+++ precache-manifest.3b13000adb9116577b21bf34541ec40f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77f306245d57b122552154a753557fa1",
+    "url": "/kddcup-intrusion-detection-system/index.html"
+  },
+  {
+    "revision": "93be559d7f0349bdd478",
+    "url": "/kddcup-intrusion-detection-system/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6d114584ce80d85308d8",
+    "url": "/kddcup-intrusion-detection-system/static/js/2.1bc1d104.chunk.js"
+  },
+  {
+    "revision": "93be559d7f0349bdd478",
+    "url": "/kddcup-intrusion-detection-system/static/js/main.4f656f1a.chunk.js"
+  },
+  {
+    "revision": "c362c886718f59ab4e15",
+    "url": "/kddcup-intrusion-detection-system/static/js/runtime~main.3e67939e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kddcup-intrusion-detection-system/precache-manifest.811387ba437495a2389985ad083690ea.js"
+  "/kddcup-intrusion-detection-system/precache-manifest.3b13000adb9116577b21bf34541ec40f.js"
 );
 
 self.addEventListener('message', (event) => {
